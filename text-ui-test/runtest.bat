57a63d70a2ddb849cc8b8d253efac8981e7b31a0
J030104 <B11030010@mail.ntust.edu.tw>
2025-03-12 23:13:43 +0800

    Updated the semi-automated testing under windows.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,11 +3,13 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM Delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
-javac -cp ..\src\main\java\eggo -Xlint:none -d ..\bin ..\src\main\java\eggo\*.java
+REM Compile all Java files recursively
+for /r ..\src\main\java %%f in (*.java) do (
+    javac -cp ..\src\main\java -Xlint:none -d ..\bin "%%f"
+)
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -19,3 +21,11 @@ java -classpath ..\bin eggo.Eggo < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT /W
+
+IF ERRORLEVEL 1 (
+    echo Test result: FAILED
+    exit /b 1
+) ELSE (
+    echo Test result: PASSED
+    exit /b 0
+)
